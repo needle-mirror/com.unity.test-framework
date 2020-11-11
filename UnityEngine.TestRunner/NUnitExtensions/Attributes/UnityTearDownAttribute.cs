126fd20feecc59bffbb0556cc8a0f03af3b639cb
Davina Armstrong <davina@playableworlds.com>
2020-11-11 10:27:47 -0800

    Added OneTimeSetUp and OneTimeTearDown

--- UnityEngine.TestRunner/NUnitExtensions/Attributes/UnityTearDownAttribute.cs
+++ UnityEngine.TestRunner/NUnitExtensions/Attributes/UnityTearDownAttribute.cs
@@ -1,10 +1,14 @@
 using System;
-using NUnit.Framework;
 
 namespace UnityEngine.TestTools
 {
-    [AttributeUsage(AttributeTargets.Method)]
-    public class UnityTearDownAttribute : NUnitAttribute
-    {
-    }
+  [AttributeUsage(AttributeTargets.Method)]
+  public class UnityTearDownAttribute: NUnitAttribute
+  {
+  }
+
+  [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+  public class UnityOneTimeTearDownAttribute: NUnitAttribute
+  {
+  }
 }
